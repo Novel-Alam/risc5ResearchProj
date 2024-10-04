f452fd24c6f505ca4d6fff0050e697baa6d76703
Novel-Alam <nonyzeon@gmail.com>
2024-10-03 18:36:03 -0700

    created a static ram variable

--- ram.h
+++ ram.h
@@ -2,3 +2,7 @@
  * text segment starts at 0x00000000 and data segment starts at 0x10010000 
     2d array of memory (2^32 locations by 1 byte) partitioned according to correct architecture
  */
+
+
+
+uint8_t ram[UINT32_MAX][1];
